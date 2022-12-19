d358925864b58bc35607edc1b80ac828dda86ee2
Allen <allenwalker1603@gmail.com>
2022-12-19 17:12:01 +0100

    added player data

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <img src={pic5} className='absolute blur-sm opacity-30 sm:opacity-50 h-full w-full object-cover' alt="" />
         <div className='relative text-white'>
           <h1 className='text-2xl md:text-5xl sm:text-6xl font-[800] underline decoration-secondary underline-offset-[12px] text-white space-x-2 p-6 tracking-widest'>01 - PROLOGUE</h1>
-          <p className='text-sm leading-8 tracking-wider p-8 sm:text-lg'>
+          <p className='text-sm leading-8 tracking-wider p-8 sm:text-lg sm:leading-[48px]'>
             <br />
             Evil Monxrchs are a premier Nigerian based esports organization dedicated to building championship caliber esports teams competing in the most successful new video games. We are one of the fastest growing esports organizations in Africa, as we connect passionate fans with fun and engaging original content. Evil Monxrchs roster includes the world champions in the most popular video game titles in the world including VALORANT, PUBG, Apex Legends and Call of Duty.
             <br />
